98f6dc0925a13b3fe5b37d55ada5bb954dbe2e97
Cloud Chen <cloudchen@verizonmedia.com>
2019-08-18 20:46:51 +0800

    feat: add onboarding screen

--- app/src/main/java/com/cloudchen/demo/MainActivity.kt
+++ app/src/main/java/com/cloudchen/demo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.cloudchen.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "override onResume() method.")
     }
 }
